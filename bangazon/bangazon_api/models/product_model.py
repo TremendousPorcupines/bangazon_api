46a365133a34f1684e00f813f661300100ea5e3c
Greg Korte <gregmkorte@gmail.com>
2017-03-03 09:13:27 -0600

    Added ProductType import to product_model.py.

--- bangazon/bangazon_api/models/product_model.py
+++ bangazon/bangazon_api/models/product_model.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from .product_type_model import ProductType
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
