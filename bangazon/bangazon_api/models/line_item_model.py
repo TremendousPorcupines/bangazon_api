6004df2841019b66ffb58b28580f132d14f18465
Greg Korte <gregmkorte@gmail.com>
2017-03-03 09:24:41 -0600

    Added models prefix to foreign keys in the LineItem model.

--- bangazon/bangazon_api/models/line_item_model.py
+++ bangazon/bangazon_api/models/line_item_model.py
@@ -4,5 +4,5 @@ from .product_model import Product
 from .order_model import Order
 
 class LineItem(models.Model):
-    order = ForeignKey(Order, on_delete=models.CASCADE,)
-    product = ForeignKey(Product, on_delete=models.CASCADE,)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE,)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,)
