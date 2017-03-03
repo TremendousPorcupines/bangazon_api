c5e9590df1eef10326e32c39e9e465b43d9fd8e5
Greg Korte <gregmkorte@gmail.com>
2017-03-03 09:09:56 -0600

    Added imports for line_item_model.

--- bangazon/bangazon_api/models/line_item_model.py
+++ bangazon/bangazon_api/models/line_item_model.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from .product_model import Product
+from .order_model import Order
+
 class LineItem(models.Model):
     order = ForeignKey(Order, on_delete=models.CASCADE,)
     product = ForeignKey(Product, on_delete=models.CASCADE,)
