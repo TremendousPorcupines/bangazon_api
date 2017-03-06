29b4ad584808202e316f4595aab05fd97d9fa6de
Hannah Hall <hannahkhall13@gmail.com>
2017-03-06 12:00:13 -0600

    fix class name

--- bangazon/bangazon_api/views/payment_view.py
+++ bangazon/bangazon_api/views/payment_view.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from serializers.payment_type_serializer import PaymentTypeSerializer
 from models.payment_type_model import PaymentTypeModel
 
-class CustomerViewSet(viewsets.ModelViewSet):
+class PaymentTypeViewSet(viewsets.ModelViewSet):
 
     queryset = PaymentTypeModel.objects.all()
     serializer_class = PaymentTypeSerializer
