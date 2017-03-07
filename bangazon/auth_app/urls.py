9e08c031b3fc6a4c64983339f86545a596de462f
Hannah Hall <hannahkhall13@gmail.com>
2017-03-07 14:47:32 -0600

    url for is_auth view" "

--- bangazon/auth_app/urls.py
+++ bangazon/auth_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^login/', views.login_user, name='login'),
     url(r'^register/', views.register_user, name='register'),
-    url(r'^logout/', views.logout_user, name='logout')
+    url(r'^logout/', views.logout_user, name='logout'),
+    url(r'^is_auth/', views.userAuth, name='is_auth')
 ]
